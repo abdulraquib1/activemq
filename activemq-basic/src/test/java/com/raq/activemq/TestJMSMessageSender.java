28645e07dae033a74d0ea5c7a73463bc4657d0d2
Abdul <araquib@ftdi.com>
2016-12-09 17:47:16 +0530

    Fixed JUnit Test Case with Running Broker

--- activemq-basic/src/test/java/com/raq/activemq/TestJMSMessageSender.java
+++ activemq-basic/src/test/java/com/raq/activemq/TestJMSMessageSender.java
@@ -1,7 +1,8 @@
 package com.raq.activemq;
 
-import static org.junit.Assert.*;
+import javax.jms.Queue;
 
+import org.apache.activemq.command.ActiveMQQueue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +14,40 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 //https://github.com/junit-team/junit4/wiki/Test-runners
 //https://maven.apache.org/guides/introduction/introduction-to-the-standard-directory-layout.html
 
+/**
+ * Note: Prior to running this test
+ * cd ACTIVEMQ_INSTALL_FOLDER\bin
+ * Run activemq start
+ * 
+ * To validate the messages posted check
+ * http://127.0.0.1:8161/admin/queues.jsp
+ *  
+ * @author araquib
+ *
+ */
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:jms-context.xml" })
+@ContextConfiguration(locations = {"classpath:test-jms-context.xml" })
 public class TestJMSMessageSender {
 	
 	@Autowired
 	private JMSMessageSender jmsMessageSender;
 
+	 // send to default destination
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
+	public void testSendDefaultDest() {
+	    jmsMessageSender.send("hello JMS");
 	}
 
+	@Test
+	public void testSendAnotherDest() {
+		
+		  // send to a code specified destination
+	    Queue queue = new ActiveMQQueue("AnotherDest");
+	    jmsMessageSender.send(queue, "hello Another Message");
+	   
+	}
+	
+	
 }
